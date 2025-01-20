3e255de60a2b549a63768c7b791cf2badfa2cc8b
Michael Herrmann <michael@herrmann.io>
2025-01-20 10:46:12 +0100

    Delete BraveUpdater/ dir on macOS

--- uninstall-brave.sh
+++ uninstall-brave.sh
@@ -7,3 +7,5 @@ sudo rm -rf /Applications/Brave\ Browser\ Nightly.app
 sudo /Library/Application\ Support/BraveSoftware/BraveUpdater/Current/BraveUpdater.app/Contents/MacOS/BraveUpdater --uninstall --system
 ~/Library/Application\ Support/BraveSoftware/BraveUpdater/Current/BraveUpdater.app/Contents/MacOS/BraveUpdater --uninstall
 
+sudo rm -rf /Library/Application\ Support/BraveSoftware/BraveUpdater
+rm -rf ~/Library/Application\ Support/BraveSoftware/BraveUpdater
