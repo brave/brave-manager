afff2982873fd0e1beccd919baa0b606c3abf63f
Michael Herrmann <michael@herrmann.io>
2025-05-27 08:30:48 +0200

    Extract module impl.actions

---
+++ macos/impl/actions.py
@@ -0,0 +1,50 @@
+from impl import brave, cache
+from impl.util import install_dmg, print_done, FileDownloader
+from os.path import exists
+from tqdm import tqdm
+
+class Uninstall:
+    def __init__(self, channel):
+        self.channel = channel
+    def __str__(self):
+        return f'Uninstall {self.channel.title()}'
+    def __call__(self):
+        with print_done(f'Uninstalling {self.channel.title()}'):
+            brave.uninstall(self.channel)
+
+class Install:
+    def __init__(self, channel, version, dmg_url):
+        self.channel = channel
+        self.version = version
+        self.dmg_url = dmg_url
+    def __str__(self):
+        return f'Install {self.channel.title()} {self.version}'
+    def __call__(self):
+        cache_path = cache.prepare(self.dmg_url.split('//', 1)[1])
+        if not exists(cache_path):
+            download_file(self.dmg_url, cache_path)
+        with print_done(f'Installing {self.channel.title()}'):
+            install_dmg(cache_path)
+
+class Launch:
+    def __init__(self, channel):
+        self.channel = channel
+    def __str__(self):
+        return f'Launch {self.channel.title()}'
+    def __call__(self):
+        brave.launch(self.channel)
+
+class ClearCache:
+    def __str__(self):
+        return 'Clear the cache'
+    def __call__(self):
+        cache.clear()
+
+def download_file(url, path):
+    print(f'Downloading {url}:')
+    downloader = FileDownloader(url, path)
+    total_size = downloader.start()
+    progress_bar = tqdm(total=total_size, unit='iB', unit_scale=True)
+    for num_bytes in downloader.run():
+        progress_bar.update(num_bytes)
+    progress_bar.close()
--- macos/main.py
+++ macos/main.py
@@ -1,8 +1,7 @@
 from impl import brave, cache, CHANNELS
+from impl.actions import Uninstall, Install, Launch, ClearCache
 from impl.releases import get_releases
-from impl.util import select, FileDownloader, install_dmg, print_done
-from os.path import exists
-from tqdm import tqdm
+from impl.util import select
 
 MAX_NUM_CHOICES_SUPPORTED_BY_QUESTIONARY_SELECT = 36
 
@@ -40,43 +39,6 @@ def main():
                 action()
     except KeyboardInterrupt:
         pass
-
-class Uninstall:
-    def __init__(self, channel):
-        self.channel = channel
-    def __str__(self):
-        return f'Uninstall {self.channel.title()}'
-    def __call__(self):
-        with print_done(f'Uninstalling {self.channel.title()}'):
-            brave.uninstall(self.channel)
-
-class Install:
-    def __init__(self, channel, version, dmg_url):
-        self.channel = channel
-        self.version = version
-        self.dmg_url = dmg_url
-    def __str__(self):
-        return f'Install {self.channel.title()} {self.version}'
-    def __call__(self):
-        cache_path = cache.prepare(self.dmg_url.split('//', 1)[1])
-        if not exists(cache_path):
-            download_file(self.dmg_url, cache_path)
-        with print_done(f'Installing {self.channel.title()}'):
-            install_dmg(cache_path)
-
-class Launch:
-    def __init__(self, channel):
-        self.channel = channel
-    def __str__(self):
-        return f'Launch {self.channel.title()}'
-    def __call__(self):
-        brave.launch(self.channel)
-
-class ClearCache:
-    def __str__(self):
-        return 'Clear the cache'
-    def __call__(self):
-        cache.clear()
 
 def ask_main_action():
     message = 'What do you want to do?'
@@ -160,15 +122,6 @@ def ask_confirm_actions(actions):
     choices = ['yes', 'no']
     choice = select(message, choices)
     return choice == choices[0]
-
-def download_file(url, path):
-    print(f'Downloading {url}:')
-    downloader = FileDownloader(url, path)
-    total_size = downloader.start()
-    progress_bar = tqdm(total=total_size, unit='iB', unit_scale=True)
-    for num_bytes in downloader.run():
-        progress_bar.update(num_bytes)
-    progress_bar.close()
 
 if __name__ == "__main__":
     main()
