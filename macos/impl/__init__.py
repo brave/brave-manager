54d4109ece2d22f21fe9bb5de5e673a99a5953b9
Michael Herrmann <michael@herrmann.io>
2025-07-31 10:44:53 +0200

    Suppress NotOpenSSLWarning on launch

--- macos/impl/__init__.py
+++ macos/impl/__init__.py
@@ -1,1 +1,6 @@
+import warnings
+
 CHANNELS = ('nightly', 'beta', 'release')
+
+# macOS uses LibreSSL. Suppress the spurious associated warning.
+warnings.filterwarnings('ignore', message='.*urllib3 v2 only supports OpenSSL')
