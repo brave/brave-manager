ccf261410f76f1a024147947268eba5b9c875d0a
Michael Herrmann <michael@herrmann.io>
2026-09-23 10:30:32 +0200

    Remove the legacy Windows scripts

--- windows/Stop BraveUpdate.bat
+++
@@ -1,4 +0,0 @@
-taskkill /F /IM BraveUpdate.exe
-taskkill /F /IM BraveCrashhandler.exe
-taskkill /F /IM BraveCrashhandler64.exe
-pause
--- windows/Uninstall Brave.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-python %~dp0uninstall_brave.py
-
-pause
